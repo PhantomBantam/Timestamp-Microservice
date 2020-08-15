56e9046b956d425d74897eea290d1a99ceb4e0f7
PhantomBantam <lucnguyen1416@gmail.com>
2020-08-15 12:58:08 -0700

    finished timestamp api

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 
+const PORT = process.env.PORT || 3000;
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -18,15 +20,17 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_string?", function (req, res) {
+  console.log(req.params.date_string);
+  let date = new Date(req.params.date_string);
+  console.log(date);
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(PORT, function () {
+  console.log('Your app is listening on port ' + PORT);
 });
